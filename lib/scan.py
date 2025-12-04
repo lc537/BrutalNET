8f16e41754f67a7800fd7ab1ac66dfd7e81b5728
cybermad <171419864+cybermads@users.noreply.github.com>
2025-12-04 21:00:00 +0900

    Update scan.py

--- lib/scan.py
+++ lib/scan.py
@@ -46,8 +46,9 @@ $R@i.~~ !     :   ~$$$$$B$$en:``
           """)
 def scan():
     banner()
-    iface = input(f"[{c}+{r}] interface  {c}:{r} ")
+    iface = input(f"[{c}+{r}] interface{c}:{r} ")
     os.system(f"arp-scan -l -I {iface}")
 
     input(f"[{c}+{r}] Finish{c}...{r}")
+
 
